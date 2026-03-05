bec4cfadb91c109c43e7ea97ddf2ac794a986b67
Michael Pasyechnyk <mic77p@gmail.com>
2026-03-04 20:24:16 -0800

    Intro updated

--- kf-prod/components/Intro.tsx
+++ kf-prod/components/Intro.tsx
@@ -1,7 +1,32 @@
 import Link from "next/link";
-import { BsCameraReels, BsCameraReelsFill } from "react-icons/bs";
+import {
+  BsCameraReels,
+  BsCameraReelsFill,
+  BsChevronCompactDown,
+  BsChevronDoubleDown,
+} from "react-icons/bs";
 import { LuClapperboard } from "react-icons/lu";
 import { PiFilmReel } from "react-icons/pi";
+import { LogoLoop } from "./LogoLoop";
+import { CurvedLoop } from "./CurvedLoop";
+
+const imageLogos = [
+  {
+    src: "/logos/tire.png",
+    alt: "Company 1",
+    href: "https://company1.com",
+  },
+  {
+    src: "/logos/montana.jpg",
+    alt: "Company 2",
+    href: "https://company2.com",
+  },
+  {
+    src: "/logos/company3.png",
+    alt: "Company 3",
+    href: "https://company3.com",
+  },
+];
 
 export default function Intro() {
   return (
@@ -17,15 +42,23 @@ export default function Intro() {
       </div>
 
       {/* Dark Overlay */}
-      {/* <div className="absolute inset-0 bg-black/50" /> */}
+      <div className="absolute inset-0 bg-black/60" />
 
       {/* Bottom fade */}
       <div className="pointer-events-none absolute bottom-0 left-0 w-full h-24 bg-gradient-to-b from-transparent to-black" />
 
-      <div className="items-center z-40 w-150 h-fit flex flex-col gap-4 p-8 pb-12">
-        {/* <h1 className="text-white text-5xl text-shadow-[0_2px_3px_rgba(0,0,0,0.5)]">
+      <div className="items-center z-40 h-fit w-full flex flex-col gap-4 p-8 pb-12">
+        <h1 className="text-white font-montserrat text-6xl text-shadow-[0_2px_3px_rgba(0,0,0,0.5)]">
           K&F Video Production
-        </h1> */}
+        </h1>
+        <p className="font-montserrat text-[18px] text-white font-normal text-center text-shadow-[0_2px_3px_rgba(0,0,0,0.5)] tracking-normal">
+          Cinematic Video Production for Brands & Creators
+        </p>
+        {/* <p className="font-sans text-[18px] text-white text-center text-shadow-[0_2px_3px_rgba(0,0,0,0.5)] tracking-normal">
+          A Toronto-based studio crafting cinematic stories that help brands
+          showcase their strengths, connect with audiences, and stand out
+          through powerful visual storytelling
+        </p> */}
         <div className="flex flex-row gap-6">
           <Link href="/" className="btn-primary px-12 gap-3">
             <BsCameraReels size={18} />
@@ -36,11 +69,31 @@ export default function Intro() {
             Projects
           </Link>
         </div>
-        {/* <p className="font-sans text-[18px] text-white text-shadow-[0_2px_3px_rgba(0,0,0,0.5)] tracking-normal">
-          A Toronto-based studio crafting cinematic stories that help brands
-          showcase their strengths, connect with audiences, and stand out
-          through powerful visual storytelling
-        </p> */}
+
+        {/* <CurvedLoop
+          marqueeText="Be ✦ Creative ✦ With ✦ React ✦ Bits ✦"
+          speed={2}
+          curveAmount={70}
+          direction="right"
+          className="custom-text-style text-6xl w-full"
+        /> */}
+
+        {/* <LogoLoop
+          logos={imageLogos}
+          speed={100}
+          direction="left"
+          logoHeight={60}
+          gap={60}
+          hoverSpeed={0}
+          scaleOnHover
+          fadeOut
+          fadeOutColor="transparent"
+          ariaLabel="Technology partners"
+        /> */}
+        {/* <BsChevronDoubleDown
+          className="text-white animate-bounce scale-x-220"
+          size={30}
+        /> */}
       </div>
     </div>
   );
